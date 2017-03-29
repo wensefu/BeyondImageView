685313a00dd96898dc7be80089036d03c0d4afbd
wensefu <wensefu@foxmail.com>
2017-03-29 22:30:58 +0800

    完成双击缩放功能

--- library/src/main/java/com/beyondsw/widget/BeyondImageView.java
+++ library/src/main/java/com/beyondsw/widget/BeyondImageView.java
@@ -1,6 +1,10 @@
 package com.beyondsw.widget;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -12,12 +16,17 @@ import com.beyondsw.widget.gesture.GestureHelper;
 /**
  * Created by wensefu on 17-3-28.
  */
-public class BeyondImageView extends ImageView{
-    
+public class BeyondImageView extends ImageView {
+
     private static final String TAG = "BeyondImageView";
     private static final boolean LOG_ENABLE = true;
+    private static final float SCALE_FACTOR = 2.5f;
+    private float mScaleFactor = 1f;
     private GestureHelper mGestureHelper;
     private Matrix mMatrix;
+    private boolean mScaling;
+    private float mPx;
+    private float mPy;
 
     public BeyondImageView(Context context) {
         this(context, null);
@@ -28,23 +37,64 @@ public class BeyondImageView extends ImageView{
         init();
     }
 
-    private void init(){
+    private void init() {
         mMatrix = new Matrix();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.concat(mMatrix);
+        super.onDraw(canvas);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             if (mGestureHelper == null) {
-                mGestureHelper = new GestureHelper(getContext(),getGestureListener());
+                mGestureHelper = new GestureHelper(getContext(), getGestureListener());
             }
         }
         return mGestureHelper.onTouchEvent(event);
     }
 
+    private void doDoubleTabScale(float x,final float y) {
+        if (mScaling) {
+            return;
+        }
+        mScaling = true;
+        final boolean scaleBigger = Float.compare(mScaleFactor, 1) == 0;
+        final float targetScale = scaleBigger ? SCALE_FACTOR : 1f;
+        final float px;
+        final float py;
+        if (scaleBigger) {
+            mPx = px = x;
+            mPy = py = y;
+        } else {
+            px = mPx;
+            py = mPy;
+        }
+        final ValueAnimator animator = ValueAnimator.ofFloat(mScaleFactor, targetScale).setDuration(200);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float)animation.getAnimatedValue();
+                mMatrix.setScale(value, value, px, py);
+                invalidate();
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mScaleFactor = targetScale;
+                mScaling = false;
+            }
+        });
+        animator.start();
+    }
+
     private GestureHelper.Listener getGestureListener() {
         return new GestureHelper.Listener() {
-            
+
             @Override
             public void onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
                 log(TAG, "onScroll: distanceX=" + distanceX + ",distanceY=" + distanceY);
@@ -58,6 +108,7 @@ public class BeyondImageView extends ImageView{
             @Override
             public void onDoubleTap(MotionEvent e) {
                 log(TAG, "onDoubleTap");
+                doDoubleTabScale(e.getX(), e.getY());
             }
 
             @Override
@@ -76,9 +127,9 @@ public class BeyondImageView extends ImageView{
             }
         };
     }
-    
-    private static void log(String TAG,String msg){
-        if(LOG_ENABLE){
+
+    private static void log(String TAG, String msg) {
+        if (LOG_ENABLE) {
             Log.d(TAG, msg);
         }
     }
